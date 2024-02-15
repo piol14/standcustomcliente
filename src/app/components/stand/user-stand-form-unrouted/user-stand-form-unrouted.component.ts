f168eb3bf5cdbee75c1893f5f2b4367332207daa
piol14 <eof812@gmail.com>
2024-02-15 11:45:38 +0100

    cositas

---
+++ src/app/components/stand/user-stand-form-unrouted/user-stand-form-unrouted.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-stand-form-unrouted',
+  templateUrl: './user-stand-form-unrouted.component.html',
+  styleUrls: ['./user-stand-form-unrouted.component.css']
+})
+export class UserStandFormUnroutedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
